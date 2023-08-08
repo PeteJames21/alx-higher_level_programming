b206a17e0024c688b7f31c01f1c873902b4fe793
PeteJames21 <peetjames21@gmail.com>
2023-08-08 15:58:55 +0300

    Remove trailing newline

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -10,7 +10,3 @@ def fizzbuzz():
             print("Buzz", end="")
         else:
             print(i, end="")
-        if i == 100:
-            print()
-        else:
-            print(" ", end="")
