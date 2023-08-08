87cf578413a7efa3571a95ce04ee64fe89888862
PeteJames21 <peetjames21@gmail.com>
2023-08-08 14:17:42 +0300

    Modify output if number is 0

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,15 +2,11 @@
 import random
 
 number = random.randint(-10000, 10000)
-number_abs = -1 * number if number < 0 else number
-ld = number_abs % 10
-print(f"Last digit of {number} is {ld}", end=" ")
+ld = abs(number) % 10
 
-if ld > 5:
-    print("and is greater than 5")
+if number > 5:
+    print(f'Last digit of {number} is {ld} and is greater than 5')
+elif number == 0:
+    print(f'Last digit of {number} is {ld} and is 0')
 else:
-    print("and is less than 6", end=" ")
-    if ld == 0:
-        print("and is 0")
-    else:
-        print("and not 0")
+    print(f'Last digit of {number} is -{ld} and is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/README.md
+++ 0x01-python-if_else_loops_functions/README.md
@@ -2,3 +2,6 @@
 
 ## 0-positive_or_negative.py
 Determine if a number is positive or negative
+
+## 1-last_digit.py
+Check the last digit of an integer
