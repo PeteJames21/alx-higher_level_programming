eacff22012fa5f4edd03ee12f1ce7278e1425f51
PeteJames21 <peetjames21@gmail.com>
2023-08-11 19:37:04 +0300

    Handle empty lists

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0:
-        return None
-    if idx >= len(my_list):
+    len_ = len(my_list)
+    if idx < 0 or idx >= len_ or len_ == 0:
         return None
 
     return my_list[idx]
