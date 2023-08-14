3876954bea6521dc85c1b3b837bda5bd96f798b1
PeteJames21 <peetjames21@gmail.com>
2023-08-14 23:10:41 +0300

    Refactor for clarity

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if not matrix:
-        return
-    for row in matrix:
-        last = len(row) - 1  # The index of the last element in the row.
-        for i, element in enumerate(row, start=0):
-            print("{:d}".format(element), end="")
-            if i != last:
-                print(" ", end="")
-            else:
-                print()
+    for i in matrix:
+        print(" ".join("{:d}".format(j) for j in i))
