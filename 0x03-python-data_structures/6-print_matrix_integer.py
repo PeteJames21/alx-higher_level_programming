5d736bf1ba6ba2d8d8ae94c64f87cfe604b3fec2
PeteJames21 <peetjames21@gmail.com>
2023-08-14 23:08:36 +0300

    Add null check

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
     for row in matrix:
         last = len(row) - 1  # The index of the last element in the row.
         for i, element in enumerate(row, start=0):
