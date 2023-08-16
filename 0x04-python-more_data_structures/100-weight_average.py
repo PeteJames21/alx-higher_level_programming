6c352b33b63cd5c62f22de237676158e443279ab
PeteJames21 <peetjames21@gmail.com>
2023-08-16 22:36:13 +0300

    Handle the case of empty list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,9 @@
 
 def weight_average(my_list=[]):
     """Return weighted average integers tuple (<score>, <weight>) in a list."""
+    if not my_list:
+        return 0
+
     total_weight = 0
     weighted_sum = 0
     for score, weight in my_list:
