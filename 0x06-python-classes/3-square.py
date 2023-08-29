16a8cf76457f5967c37297e087e9e8480f382e69
PeteJames21 <peetjames21@gmail.com>
2023-08-29 22:53:24 +0300

    Add method for calculating area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
 """Defines a Square type."""
 
 
@@ -17,3 +17,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+        """Return the squared area of the Square object."""
+        return self.__size ** 2
