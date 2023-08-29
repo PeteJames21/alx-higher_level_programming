a42098838d60080f89867bfb4fad9f39cc690f84
PeteJames21 <peetjames21@gmail.com>
2023-08-29 22:34:37 +0300

    Use private variable in a class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a Square type."""
+
+
+class Square:
+    """Models a square shape of fixed dimensions."""
+    def __init__(self, size):
+        """Initialize a square object of a given size."""
+        self.__size = size
--- 0x06-python-classes/README.md
+++ 0x06-python-classes/README.md
@@ -2,3 +2,8 @@
 
 ## 0-square.py
 Write an empty class Square that defines a square:
+
+## 1-square.py
+Write a class Square that defines a square by:
+- Private instance attribute: `size`
+- Instantiation with size (no type/value verification)
