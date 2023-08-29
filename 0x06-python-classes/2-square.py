7b41d6e3281ef2de379fb7ef5ca4daf1e822ce96
PeteJames21 <peetjames21@gmail.com>
2023-08-29 22:48:19 +0300

    Create Square class with size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a Square type."""
+
+
+class Square:
+    """Models a square shape of fixed dimensions."""
+    def __init__(self, size=0):
+        """Initialize a square object of a given size.
+
+        Args:
+            size (int): size of the square - a positive integer >= 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
