b2a470a2f7bd5ff5f79f8a4f211c92ca9f7545f1
PeteJames21 <peetjames21@gmail.com>
2023-08-31 22:19:17 +0300

    Write class from disassembled bytecode

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+"""Defined a class that models a circle."""
 from math import pi
 
 
 class MagicClass:
-    def __init__(self, radius):
+    """Represents a circle."""
+    def __init__(self, radius=0):
         if type(radius) not in (int, float):
             raise TypeError("radius must be a number")
 
         self.__radius = radius
 
     def area(self):
+        """Return the area of the circle."""
         return pi * (self.__radius ** 2)
 
     def circumference(self):
+        """Return the circumference of the circle."""
         return 2 * pi * self.__radius
