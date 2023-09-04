efafcb5cde2466b69161913439c64f1c0d06720a
PeteJames21 <peetjames21@gmail.com>
2023-09-04 12:51:13 +0300

    Amend module docstring

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""
+Defines a Rectangle class.
+"""
 
 
 class Rectangle:
     """Models a rectangle."""
     def __init__(self, width=0, height=0):
-        """Initialize the Rectangle with the provided dimensions."""
+        """Initialize the Rectangle with the provided dimensions"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Return the width of the rectangle."""
+        """Return the width of the rectangle"""
         return self.__width
 
     @width.setter
@@ -25,12 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Return the height of the rectangle."""
+        """Return the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle."""
+        """Set the height of the rectangle"""
         if type(value) is not int:
             raise ValueError("height must be an integer")
         if value < 0:
