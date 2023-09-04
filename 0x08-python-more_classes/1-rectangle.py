0832897f362c4b46c7bb2a11753ebd0b1da5b756
PeteJames21 <peetjames21@gmail.com>
2023-09-04 12:36:47 +0300

    Redefine Rectangle class with added width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+
+
+class Rectangle:
+    """Models a rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize the Rectangle with the provided dimensions."""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Return the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width of the rectangle."""
+        if type(value) is not int:
+            raise ValueError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Return the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height of the rectangle."""
+        if type(value) is not int:
+            raise ValueError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+my_rectangle = Rectangle(2, 4)
+print(my_rectangle.__dict__)
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print(my_rectangle.__dict__)
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -2,3 +2,6 @@
 
 ## 0-rectangle.py
 Write an empty class Rectangle that defines a rectangle.
+
+## 1-rectangle.py
+Define a `Rectangle` class with a `width` and `height` atrribute, both accessible via getters and setters.
