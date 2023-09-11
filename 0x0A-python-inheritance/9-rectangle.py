f71b6707d65f05c96b45797770c678505bb471d3
PeteJames21 <peetjames21@gmail.com>
2023-09-11 18:07:55 +0300

    Add base class to the module

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,7 +3,20 @@
 Define a class that models a rectangle.
 """
 
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+class BaseGeometry:
+    """A base class for geometrical types."""
+
+    def area(self):
+        """Compute the area."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Check if a value is a valid dimension for this class."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
 
 
 class Rectangle(BaseGeometry):
