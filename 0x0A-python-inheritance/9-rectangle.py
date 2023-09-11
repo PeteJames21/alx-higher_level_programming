8b786bb54d06a0fea32acf331a1ee5b06fe7e6a8
PeteJames21 <peetjames21@gmail.com>
2023-09-11 18:10:30 +0300

    Remove the print method

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -3,20 +3,7 @@
 Define a class that models a rectangle.
 """
 
-
-class BaseGeometry:
-    """A base class for geometrical types."""
-
-    def area(self):
-        """Compute the area."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Check if a value is a valid dimension for this class."""
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -35,7 +22,3 @@ class Rectangle(BaseGeometry):
     def __str__(self):
         """Return the string representation of a Rectangle instance."""
         return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def print(self):
-        """Print the string representation of a Rectangle instance."""
-        print(self.__str__())
