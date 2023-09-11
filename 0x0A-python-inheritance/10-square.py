350a1db556240e41563363db0b1198e7e682e18b
PeteJames21 <peetjames21@gmail.com>
2023-09-11 18:19:55 +0300

    Add method and class docstrings

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,7 +7,9 @@ Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
+    """Models a square shape."""
     def __init__(self, size):
+        """Initialize an instance of this class."""
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
