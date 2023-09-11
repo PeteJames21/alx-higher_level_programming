376a30af7f51f3dfe0ac7edc5732e543f902f49f
PeteJames21 <peetjames21@gmail.com>
2023-09-11 19:30:36 +0300

    Implement the MyInt class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,13 +5,13 @@ and __ne__ methods.
 """
 
 
-def MyInt(int):
+class MyInt(int):
     """A subclass of int that inverts the __eq__ and __ne__ methods."""
 
     def __eq__(self, other):
-        """An inversion of int.__eq__."""
-        return not super().__eq__(self, other)
+        """Invert the behavior of __eq__"""
+        return not super().__eq__(other)
 
     def __ne__(self, other):
-        """An inversion of int.__ne__."""
-        return not super().__ne__(self, other)
+        """Invert the behavior of __ne__"""
+        return not super().__ne__(other)
