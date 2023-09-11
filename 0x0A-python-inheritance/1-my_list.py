daa5576d7cab271112c1e11dd9125908ceb12d4a
PeteJames21 <peetjames21@gmail.com>
2023-09-11 16:28:37 +0300

    Remove __init__

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,10 +8,6 @@ includes a method for printing its sorted elements.
 class MyList(list):
     """An extension of the builtin list type."""
 
-    def __init__(self, *args, **kwargs):
-        """Initialize an instance of this class."""
-        super().__init__(*args, **kwargs)
-
     def print_sorted(self):
         """Print the list, sorted in ascending order."""
         print(sorted(self))
