ad39637b073471c5e7a15d74b42ed893f5529af4
PeteJames21 <peetjames21@gmail.com>
2023-09-12 21:33:04 +0300

    Refactor to return __dict__ directly

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,10 +7,4 @@ Define a class for serializing the attributes of an object.
 
 def class_to_json(obj):
     """Serialize the attributes of obj into a dict."""
-    d = {}
-    for key, value in obj.__dict__.items():
-        # Do not serialize private and special atrributes.
-        if not key.startswith("__"):
-            d[key] = value
-
-    return d
+    return obj.__dict__
