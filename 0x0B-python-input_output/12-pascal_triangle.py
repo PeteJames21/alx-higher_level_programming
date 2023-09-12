4933d765941ef30a3b97fe9f425ef52b3e260d02
PeteJames21 <peetjames21@gmail.com>
2023-09-12 22:55:27 +0300

    Create Pascal's triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Define a function for creating Pascal's triangle.
+"""
+
+
+def factorial(n):
+    """Return the factorial of n."""
+    if n < 0:
+        raise ValueError("n must be >= 0")
+
+    if (n == 0 or n == 1):
+        return 1
+
+    else:
+        return (n * factorial(n - 1))
+
+
+def pascal_triangle(n):
+    """Generate a list of lists representing pascal's triangle of size n."""
+    triangle = []
+    for i in range(0, n):
+        row = []
+        for j in range(0, i + 1):
+            term = factorial(i) / (factorial(j) * factorial(i - j))
+            row.append(int(term))  # Convert float to int
+
+        triangle.append(row)
+
+    return triangle
