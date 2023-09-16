0f4e376eb7d80a49acc05c4cecfb697fd2bbd829
PeteJames21 <peetjames21@gmail.com>
2023-09-16 12:33:22 +0300

    Add a getter and setter for all attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -25,6 +25,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the width."""
+        self.validate_attribute("width", value)
         self.__width = value
 
     @property
@@ -35,6 +36,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the height."""
+        self.validate_attribute("height", value)
         self.__height = value
 
     @property
@@ -45,6 +47,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the value of the x coordinate."""
+        self.validate_attribute("x", value)
         self.__x = value
 
     @property
@@ -55,4 +58,18 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the value of the y coordinate."""
+        self.validate_attribute("y", value)
         self.__y = value
+
+    @staticmethod
+    def validate_attribute(name, value):
+        """Check if an attribute has a valid value."""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if name in ["x", "y"]:
+            if value < 0:
+                raise ValueError(f"{name} must be >= 0")
+        elif name in ["width", "height"]:
+            if value <= 0:
+                raise ValueError(f"{name} must be > 0")
+        # Invalid attribute names are not handled
