f03697c1e675641263a5fa16947ef6e01ad8bc43
PeteJames21 <peetjames21@gmail.com>
2023-10-17 22:53:48 +0300

    -Enclose field names in back ticks

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- Get counts of unique score values.
 
-SELECT score, count(*) AS "number"
-FROM second_table
-GROUP BY score;
-ORDER BY number DESC;
+SELECT `score`, count(*) AS `number`
+FROM `second_table`
+GROUP BY `score`;
+ORDER BY `number` DESC;
