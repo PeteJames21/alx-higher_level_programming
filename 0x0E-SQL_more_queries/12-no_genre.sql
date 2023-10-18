7af9300dd0279f95c9e9f0da4fbb2cfdbbbab14b
PeteJames21 <peetjames21@gmail.com>
2023-10-18 22:56:03 +0300

    List all show titles that do not have a genre linked

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- List all show titles that do not have a genre linked
+
+SELECT shows.title, genres.genre_id
+FROM tv_shows AS shows
+    LEFT JOIN tv_show_genres AS genres
+    ON shows.id = genres.show_id 
+WHERE genres.genre_id IS NULL
+ORDER BY shows.title, genres.genre_id;
