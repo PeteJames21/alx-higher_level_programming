99da178742c16e20561282aa725a92d095871f2e
PeteJames21 <peetjames21@gmail.com>
2023-10-18 23:43:41 +0300

    Reorder the join queries

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
 -- Display all comedy shows
 
 SELECT tv_shows.title
-FROM tv_genres
+FROM tv_shows
     INNER JOIN tv_show_genres
-    ON tv_genres.id = tv_show_genres.genre_id
+        ON tv_shows.id = tv_show_genres.show_id
 
-    INNER join tv_shows
-    ON tv_show_genres.show_id = tv_shows.id
+    INNER JOIN tv_genres
+        ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
