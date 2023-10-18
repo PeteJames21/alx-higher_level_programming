681e1e8b1081e47db3b0339f1aa872288b488c2e
PeteJames21 <peetjames21@gmail.com>
2023-10-18 22:31:24 +0300

    list all shows with at least one genre linked

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+-- list all shows with at least one genre linked
+
+SELECT s.title, g.genre_id
+  FROM tv_shows AS s
+        INNER JOIN tv_show_genres AS g
+	ON s.id = g.show_id
+ ORDER BY s.title, g.genre_id;
