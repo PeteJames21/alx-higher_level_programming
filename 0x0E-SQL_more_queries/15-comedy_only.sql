2a32dbdb00acf0351137545bcf6d0bcef23c961a
PeteJames21 <peetjames21@gmail.com>
2023-10-18 23:39:16 +0300

    Change indentation

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,9 +3,9 @@
 SELECT tv_shows.title
 FROM tv_genres
     INNER JOIN tv_show_genres
-        ON tv_genres.id = tv_show_genres.genre_id
+    ON tv_genres.id = tv_show_genres.genre_id
 
     INNER join tv_shows
-        ON tv_show_genres.show_id = tv_shows.id
+    ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title ASC;
