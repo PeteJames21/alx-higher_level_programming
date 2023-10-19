5efc2c3e2a0bf3538fbe9ea831f79e365422714c
PeteJames21 <peetjames21@gmail.com>
2023-10-19 11:57:15 +0300

    Select genres not linked to Dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -2,13 +2,13 @@
 
 SELECT DISTINCT tv_genres.name
 FROM tv_genres
-    INNER JOIN (
+WHERE tv_genres.id NOT IN
+    (
+        -- Select genre ids linked to Dexter
         SELECT tv_show_genres.genre_id
         FROM tv_show_genres
             INNER JOIN tv_shows
                 ON tv_show_genres.show_id = tv_shows.id
-        WHERE tv_shows.title <> "Dexter"
-    ) AS t
-        ON t.genre_id = tv_genres.id
-ORDER BY tv_genres.name
-;
+        WHERE tv_shows.title = "Dexter"
+    )
+ORDER BY tv_genres.name;
