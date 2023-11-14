36179cc259411f3e5bdad238b385c58eee48aa35
PeteJames21 <peetjames21@gmail.com>
2023-11-14 11:52:06 +0300

    Refactor: remove usage of .length

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 // Print the first arg passed to the script
 
 const args = process.argv;
-if (args.length === 2) {
-  console.log('No argument');
-} else {
+if (args[2]) {
   console.log(args[2]);
+} else {
+  console.log('No argument');
 }
