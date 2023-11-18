e139afd8f78c383d455e071be22d4740973cbc08
PeteJames21 <peetjames21@gmail.com>
2023-11-18 12:29:33 +0300

    Search for a state in the database

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+List all states in the database whose name matches that provided by the user.
+    Usage: <username> <password> <databaseName> <stateName>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    # Get username and password
+    if len(sys.argv) != 5:
+        print("Usage: <username> <password> <databaseName> <stateName>")
+        exit(1)
+    uname = sys.argv[1]
+    pwd = sys.argv[2]
+    db_name = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(user=uname, password=pwd, database=db_name)
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name='{}' ORDER BY id".format(name)
+    )
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+    db.close()
