eb01ed121f1d03728e337679e55be8c41db94ee2
PeteJames21 <peetjames21@gmail.com>
2023-11-30 18:52:21 +0300

    Refactor

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
-# Send a request to a URL and display the size of the body of the response in bytes.
-curl -sI "$1" | grep Content-Length | cut --delimiter=" " -f 2
+#!/bin/bash
+# Display size of body of a response in bytes
+curl -s "$1" | wc -c
