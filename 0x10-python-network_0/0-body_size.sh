41df6d00f7f71cda9cb017931b7322770e7a4860
PeteJames21 <peetjames21@gmail.com>
2023-11-30 18:50:48 +0300

    Change Shebang to meet ALX requirements

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Send a request to a URL and display the size of the body of the response in bytes.
 curl -sI "$1" | grep Content-Length | cut --delimiter=" " -f 2
