87c89f4b4e883430c6968caea4a03e532576b4b1
PeteJames21 <peetjames21@gmail.com>
2024-01-03 14:44:17 +0300

    Write a string to a file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Write a string to a file. The filaname is the first argument to this script
+// The string to write is the second arg.
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const contentToWrite = process.argv[3];
+
+if (!filePath || !contentToWrite) {
+  console.error('Usage: ./1-writeme.js FILENAME STRING');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, contentToWrite, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
