fb96e0f6bfe0b3d401d34fe0b5eda27abb8454ef
PeteJames21 <peetjames21@gmail.com>
2024-01-03 15:18:36 +0300

    Refactor

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,27 +2,17 @@
 // Compute and print the number of tasks completed by user id.
 
 const request = require('request');
-const apiUrl = process.argv[2];
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  try {
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
     const todos = JSON.parse(body);
     const completed = {};
-
     todos.forEach((todo) => {
-      if (todo.completed) {
-        completed[todo.userId] = (completed[todo.userId] || 0) + 1;
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-
     console.log(completed);
-  } catch (parseError) {
-    console.error('Error parsing JSON:', parseError);
   }
 });
-
